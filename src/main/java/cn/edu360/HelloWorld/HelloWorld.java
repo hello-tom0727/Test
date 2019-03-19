b38ae4e3014070ca5b4d2bc4d654333ecf1749b7
Tom <lu.haipeng2@ztesoft.com>
2019-03-19 22:44:33 +0800

    add HelloWorld

---
+++ src/main/java/cn/edu360/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.edu360.HelloWorld;
+
+/**
+ * @Author：luhaipeng
+ * @Description：
+ * @Date：2019/3/19
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
